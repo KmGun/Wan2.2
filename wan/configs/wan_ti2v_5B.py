7be00c34eba49afd425d2bbad41f91a33702a080
KmGun <gunpoll823@gmail.com>
2025-08-17 12:36:45 +0900

    1236

--- wan/configs/wan_ti2v_5B.py
+++ wan/configs/wan_ti2v_5B.py
@@ -27,8 +27,11 @@ ti2v_5B.window_size = (-1, -1)
 ti2v_5B.qk_norm = True
 ti2v_5B.cross_attn_norm = True
 ti2v_5B.eps = 1e-6
+ti2v_5B.low_noise_checkpoint = 'low_noise_model'
+ti2v_5B.high_noise_checkpoint = 'high_noise_model'
 
 # inference
+ti2v_5B.boundary = 0.875
 ti2v_5B.sample_fps = 24
 ti2v_5B.sample_shift = 5.0
 ti2v_5B.sample_steps = 50
